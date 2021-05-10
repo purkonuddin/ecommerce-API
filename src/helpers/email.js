24948c0db405dc1a32260747c93e52d8470ff9dc
purkonuddin <purkonud12119617@gmail.com>
2021-05-11 00:54:42 +0700

    email

--- src/helpers/email.js
+++ src/helpers/email.js
@@ -1,18 +1,21 @@
+require('dotenv').config()
 const nodemailer = require('nodemailer')
 const helpers = require('./index')
 
 const SendEmail = (req, res, next) => {
   try {
+    const eAuth = {
+      user: process.env.EMAIL_ADDRESS,
+      pass: process.env.EMAIL_PASS
+    }
+    // console.log(auth)
     const userEmail = req.body.user_email
     const emailSubject = req.body.email_subject
     const emailContent = req.body.email_content
 
     var smtpTransport = nodemailer.createTransport({
       service: 'Gmail',
-      auth: {
-        user: 'purkonuddin25@gmail.com',
-        pass: 'ArkademyB14'
-      }
+      auth: eAuth
     })
     var mailOptions = {
       to: userEmail,
@@ -23,7 +26,7 @@ const SendEmail = (req, res, next) => {
 
     smtpTransport.sendMail(mailOptions, function (err) {
       if (err) {
-        helpers.customErrorResponse(res, 404, `akun telah dibuat, tapi gagal mengirim email verifikasi. ${err}. atau verifikasi ulang melalui halaman forget password`)
+        helpers.customErrorResponse(res, 404, `${err}`)
       }
       req.body.object = 'users'
       req.body.action = 'kirim email'
